2713351669dc61dbc9d0d76d787e0e91ff7c4063
mdobaid311 <mdobaid311@gmail.com>
2023-03-29 19:44:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Mohammed obaid';
+  imgURL =
+    'https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559__480.jpg';
+
+  getName() {
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
 }
